e0eb1829ed8e748de7af711676b25867c4fc145d
caipine <caipine@users.noreply.github.com>
2019-09-03 09:57:45 -0500

    Update 03.R

--- 03.R
+++ 03.R
@@ -1,6 +1,6 @@
 
 
-samples <- read.table("sample.list-t")[,1]
+samples <- read.table("sample.list")[,1]
 samples
 #samples <- "MCL72"
 
@@ -12,7 +12,7 @@ length(normals)
 
 
 for (i in 1:length(samples) ) {
-
+print(samples[i])
 file1 <- paste("annotated/", samples[i], "/gene_table_", samples[i], "_to_", normals[1],  "_9.txt", sep="")
 X1 <- read.table(file1, sep = " ") 
 X2 <- data.frame(paste(X1$V1, X1$V2, X1$V4, X1$V5, X1$V6, sep = ":"))
@@ -23,14 +23,14 @@ colnames(X2) <- c("SNP", normals[1])
 X <- X2
 
 for (j in normals[2:length(normals)] ) {
-
+print(j)
 
 file <- paste("annotated/", samples[i], "/gene_table_", samples[i], "_to_", j ,  "_9.txt", sep="")
 X1 <- read.table(file, sep = " ") 
 X2 <- data.frame(paste(X1$V1, X1$V2, X1$V4, X1$V5, X1$V6, sep = ":"))
 X2[,2] <- 1
 colnames(X2) <- c("SNP",j)
-#print(dim(X2))
+print(dim(X2))
 X <- merge(X, X2, by="SNP")
 print(dim(X))
 }
