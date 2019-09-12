4e89a6e8169a400eee8d23b55a736efab17e18ff
caipine <caipine@users.noreply.github.com>
2019-09-12 13:50:55 -0500

    Update 03.multi-normal.R

--- 03.multi-normal.R
+++ 03.multi-normal.R
@@ -21,7 +21,8 @@ print(samples[i])
 file1 <- paste("annotated/", samples[i], "/gene_table_", samples[i], "_to_", normals[1],  "_9.txt", sep="")
 print(file1)
 X1 <- read.table(file1, sep = " ") 
-X2 <- data.frame(paste(X1$V1, X1$V2, X1$V4, X1$V5, X1$V6, sep = ":"))
+  
+X2 <- data.frame(paste(X1$V1, X1$V2, X1$V3, X1$V4, X1$V5, X1$V6, X1$V7, sep = ":")) #X1$V1, X1$V2, X1$V4, X1$V5, X1$V6
 X2[,2] <- 1
 dim(X2)
 colnames(X2) <- c("SNP", normals[1])
@@ -36,7 +37,7 @@ print(j)
 
 file <- paste("annotated/", samples[i], "/gene_table_", samples[i], "_to_", j ,  "_9.txt", sep="")
 X1 <- read.table(file, sep = " ") 
-X2 <- data.frame(paste(X1$V1, X1$V2, X1$V4, X1$V5, X1$V6, sep = ":"))
+X2 <- data.frame(paste(X1$V1, X1$V2, X1$V3, X1$V4, X1$V5, X1$V6, X1$V7, sep = ":"))
 X2[,2] <- 1
 colnames(X2) <- c("SNP",j)
 print(dim(X2))
